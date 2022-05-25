e9fcda72860b754b968e14f467226ac2cb360792
Guilherme Beidaki Peres <guibperes@protonmail.com>
2022-05-25 15:11:19 -0300

    changed docker installation method

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,6 +18,10 @@ sudo apt-get install -y \
 	git \
         xclip \
         mpv \
+	ca-certificates \
+	curl \
+	gnupg \
+	lsb-release \
         > /dev/null
 
 echo -e "# APT cleanup"
@@ -35,7 +39,20 @@ echo -e "# Starship prompt"
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
 echo -e "\n# Docker installation"
-sh -c "$(curl -fsSL https://get.docker.com)"
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update > /dev/null
+sudo apt-get install -y \
+	docker-ce \
+	docker-ce-cli \
+	containerd.io \
+	docker-compose-plugin \
+	> /dev/null
+
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
 
