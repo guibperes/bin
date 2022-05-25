db59d041205a5b973fd39a2b11988cdab1889d3b
Guilherme Beidaki Peres <guibperes@protonmail.com>
2022-05-25 13:14:31 -0300

    created configs directory, and added directory check and creation

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,8 +8,23 @@ echo -e "# Gnome Utils installation script"
 echo -e "# Clonning guibperes/bin github repository"
 git clone -q https://github.com/guibperes/bin.git $BIN_PATH
 
-echo -e "\n# Moving .XCompose file to HOME directory"
-mv $BIN_PATH/.XCompose $HOME
+echo -e "\n# Creating directories if they not exists"
+[ ! -d "$HOME/.config" ] &&\
+	echo -e "Creating \$HOME/.config directory" &&\
+	mkdir $HOME/.config
+
+[ ! -d "$HOME/.config/mpv" ] &&\
+	echo -e "Creating \$HOME/.config/mpv directory" &&\
+	mkdir $HOME/.config/mpv
+
+echo -e "\n# Copying .XCompose keyboard configuration file"
+cp $BIN_PATH/configs/.XCompose $HOME/.XCompose
+
+echo -e "\n# Copying Starship prompt configuration file"
+cp $BIN_PATH/configs/starship.toml $HOME/.config/starship.toml
+
+echo -e "\n# Copying MPV configuration file"
+cp $BIN_PATH/configs/mpv.conf $HOME/.config/mpv/mpv.conf
 
 echo -e "\n# Adding $BIN_NAME directory to your path"
 echo -e "# Add this line to your RC file (.bashrc, .zshrc, etc)\n"
