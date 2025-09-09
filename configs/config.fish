bcfba2660b03d7558e61537187a29a564073bf6a
Guilherme Beidaki Peres <guibperes@proton.me>
2025-09-09 08:06:08 -0300

    updated fish config to interactive start

--- configs/config.fish
+++ configs/config.fish
@@ -1,12 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    # Starship prompt
+    starship init fish | source
+
+    # Mise
+    mise activate fish | source
 end
-
-# Starship prompt
-starship init fish | source
-
-# Mise
-mise activate fish | source
 
 fish_add_path $HOME/.bin/cli
 set -g fish_greeting
