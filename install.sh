eae57bbedc190399819d96d446657ce1a50f0255
Guilherme Beidaki Peres <guibperes@proton.me>
2025-10-03 13:00:31 -0300

    Install: use only one cp -r for .config files

--- install.sh
+++ install.sh
@@ -57,17 +57,19 @@ mkdir -p \
   $HOME_CONFIG_PATH/wofi \
   $HOME/Pictures/wallpapers
 
-cp -r $AKIRA_CONFIG_PATH/fastfetch $HOME_CONFIG_PATH
-cp -r $AKIRA_CONFIG_PATH/fish $HOME_CONFIG_PATH
-cp -r $AKIRA_CONFIG_PATH/hypr $HOME_CONFIG_PATH
-cp -r $AKIRA_CONFIG_PATH/kitty $HOME_CONFIG_PATH
-cp -r $AKIRA_CONFIG_PATH/mpv $HOME_CONFIG_PATH
-cp -r $AKIRA_CONFIG_PATH/spotify-player $HOME_CONFIG_PATH
-cp -r $AKIRA_CONFIG_PATH/waybar $HOME_CONFIG_PATH
-cp -r $AKIRA_CONFIG_PATH/wofi $HOME_CONFIG_PATH
+cp -r \
+  $AKIRA_CONFIG_PATH/fastfetch \
+  $AKIRA_CONFIG_PATH/fish \
+  $AKIRA_CONFIG_PATH/hypr \
+  $AKIRA_CONFIG_PATH/kitty \
+  $AKIRA_CONFIG_PATH/mpv \
+  $AKIRA_CONFIG_PATH/spotify-player \
+  $AKIRA_CONFIG_PATH/waybar \
+  $AKIRA_CONFIG_PATH/wofi \
+  $AKIRA_CONFIG_PATH/starship.toml \
+  $HOME_CONFIG_PATH
 
 cp $AKIRA_CONFIG_PATH/.XCompose $HOME/.XCompose
-cp $AKIRA_CONFIG_PATH/starship.toml $HOME_CONFIG_PATH
 cp $AKIRA_DIR_PATH/assets/akira_wallpaper.jpg $HOME/Pictures/wallpapers
 
 echo -e "\n# Applying configurations"
