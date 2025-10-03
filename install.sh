933a371959612e4a89e3778000c6b479f8a3ee33
Guilherme Beidaki Peres <guibperes@proton.me>
2025-10-03 13:14:17 -0300

    Install: removed all \n from echo

--- install.sh
+++ install.sh
@@ -18,13 +18,13 @@ GIT_URL_LAZYVIM=https://github.com/guibperes/lazyvim-config
 # echo -e "\n# Akira GTK theme"
 # git clone $GIT_URL_AKIRA_GTK $HOME/.themes/Akira
 
-echo -e "\n# Starting Akira installation script"
-echo -e "\n# Pacman package manager full system update and packages install"
+echo -e "# Starting Akira installation script"
+echo -e "# Pacman package manager full system update and packages install"
 sudo cp $AKIRA_CONFIG_PATH/pacman.conf /etc/pacman.conf
 sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm - <$AKIRA_PKG_PATH/pacman.txt
 
-echo -e "\n# Yay installation, configuration and packages install"
+echo -e "# Yay installation, configuration and packages install"
 sudo pacman -S --needed --noconfirm base-devel
 git clone $YAY_URL $YAY_DIR
 cd $YAY_DIR
@@ -35,16 +35,16 @@ rm -rf $YAY_DIR
 yay -Y --gendb
 cat $AKIRA_PKG_PATH/yay.txt | xargs yay -S --noconfirm
 
-echo -e "\n# Flatpak installation"
+echo -e "# Flatpak installation"
 cat $AKIRA_PKG_PATH/flatpak.txt | xargs flatpak install --noninteractive -y flathub
 
-echo -e "\n# LazyVim install"
+echo -e "# LazyVim install"
 git clone $GIT_URL_LAZYVIM $HOME_CONFIG_PATH/nvim
 
-echo -e "\n# Mise install global tools"
+echo -e "# Mise install global tools"
 mise use -g node@22
 
-echo -e "\n# Copying configuration files"
+echo -e "# Copying configuration files"
 mkdir -p \
   $HOME_CONFIG_PATH/fastfetch \
   $HOME_CONFIG_PATH/fish \
@@ -72,14 +72,14 @@ cp -r \
 cp $AKIRA_CONFIG_PATH/.XCompose $HOME/.XCompose
 cp $AKIRA_DIR_PATH/assets/akira_wallpaper.jpg $HOME/Pictures/wallpapers
 
-echo -e "\n# Applying configurations"
+echo -e "# Applying configurations"
 sudo usermod -aG docker $USER
 sudo chsh -s /bin/fish $USER
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 kitten themes --reload-in all Akira
 fc-cache
 
-echo -e "\n# Systemctl enable and starting services"
+echo -e "# Systemctl enable and starting services"
 cat $AKIRA_PKG_PATH/systemctl-enable.txt | xargs sudo systemctl enable --now
 
-echo -e "\n# Finished Post installation script"
+echo -e "# Finished Post installation script"
