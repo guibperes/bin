b1611a46ca184a24167485ae80788c63ad69d9a1
Guilherme Beidaki Peres <guibperes@proton.me>
2025-10-07 11:24:31 -0300

    Install: changed lazyvim git url

--- scripts/env.sh
+++ scripts/env.sh
@@ -10,7 +10,7 @@ export HOME_CONFIG_PATH=$HOME/.config
 export YAY_URL=https://aur.archlinux.org/yay.git
 export YAY_DIR=$AKIRA_DIR_PATH/yay
 
-export GIT_URL_LAZYVIM=https://github.com/guibperes/lazyvim-config
+export GIT_URL_LAZYVIM=https://github.com/guibperes/akira-lazyvim
 # GIT_URL_AKIRA_GTK=https://github.com/guibperes/akira-gtk-theme
 
 export GIT_USER_NAME="Guilherme Beidaki Peres"
